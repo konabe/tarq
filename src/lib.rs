f3ba66d6a73502045edaf3fa8bc13c459010aa77
konabe <rt.konabe@gmail.com>
2025-07-19 15:14:44 +0900

    add counting classes in index.js

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
+use swc_ecma_ast::Stmt;
+use swc_ecma_ast::{ModuleItem};
 use wasm_bindgen::prelude::*;
 use serde::{Serialize};
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
+
+use swc_common::sync::Lrc;
+use swc_common::{SourceMap};
+use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
 
 
 #[derive(Serialize, Debug)]
@@ -20,7 +27,49 @@ pub fn greet(name: &str) {
 }
 
 pub fn execute() {
-    let output = Output{class_number: 10};
+    let cm: Lrc<SourceMap> = Default::default();
+    // テストのときはこれを使う
+    // let fm = cm.new_source_file(
+    //     FileName::Custom("index.js".into()).into(),
+    //     "function foo() {}",
+    // );
+    let fm = cm
+    .load_file(Path::new("index.js"))
+    .expect("failed to load test.js");
+    let lexer = Lexer::new(
+        // We want to parse ecmascript
+        Syntax::Es(Default::default()),
+        // EsVersion defaults to es5
+        Default::default(),
+        StringInput::from(&*fm),
+        None,
+    );
+    let mut parser = Parser::new_from(lexer);
+    let _module = parser
+        .parse_module()
+        .expect("failed to parser module");
+    let class_count = _module.body.iter().filter(|item| {
+        match item {
+            ModuleItem::Stmt(class_decl) => {
+                // println!("Class Name: {}", class_decl.ident.sym);
+                match class_decl {
+                        Stmt::Decl(class_decl) => {
+                            match class_decl {
+                                swc_ecma_ast::Decl::Class(_) => {
+                                    // println!("Class Name: {}", class_decl.ident.sym);
+                                    return true;
+                                },
+                                _ => return false,
+                            }
+                        },
+                        _ => return false,
+                }
+            },
+            _ => return false,
+        }
+    }).count();
+
+    let output = Output{class_number: class_count};
     let output_file_name = String::from("output.json");
     let result = write_json(&output_file_name, output);
     match result {
